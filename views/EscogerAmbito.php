79779e8636531806321169ce234561fe73ffda5e
Santiago Diaz <santidiazg@hotmail.es>
2020-06-07 15:07:21 -0700

    fix navbar link on EscogerAmbito

--- views/EscogerAmbito.php
+++ views/EscogerAmbito.php
@@ -23,7 +23,7 @@
             <a class="nav-link" href="/views/home.php">Home</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="/views/DescubrirPersonas.php">Descubrir Personas<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/views/EscogerAmbito.php">Descubrir Personas<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/views/Conversaciones.php">Conversaciones</a>
