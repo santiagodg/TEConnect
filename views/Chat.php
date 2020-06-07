dac6b93fd51ad687ebbb063022126f8bfe9493fd
Santiago Diaz <santidiazg@hotmail.es>
2020-06-07 16:49:33 -0700

    add Chat

--- views/Chat.php
+++ views/Chat.php
@@ -12,8 +12,8 @@
 
     <title>TEConnect | Descubrir Personas</title>
   </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <body style="display: flex; flex-flow: column; height: 100%;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index: 2;">
       <a class="navbar-brand" href="/index.php">TEConnect</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -34,54 +34,65 @@
       </div>
     </nav>
 
-    <div class="container-fluid">
-      <div class="row py-5">
-        <div class="col-3"></div>
-        <div class="col">
-          <div class="media py-3">
+    <div class="container-flex" style="flex: 1 1 auto; overflow-y:hidden;">
+      <div class="row" style="position: absolute; bottom: 0; left: 15px; width: 100%; height: auto; overflow-y:hidden;">
+        <div class="col-12">
+          <div class="media py-4 px-5">
             <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
             <div class="media-body">
-              <a href="/views/Chat.php?otherUser=1"><h5 class="mt-0">Santiago</h5></a>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-          <hr>
-          <div class="media py-3">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <a href="/views/Chat.php?otherUser=1"><h5 class="mt-0">Shaar</h5></a>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-          <hr>
-          <div class="media py-3">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <a href="/views/Chat.php?otherUser=1"><h5 class="mt-0">Camilo</h5></a>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-          <hr>
-          <div class="media py-3">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <a href="/views/Chat.php?otherUser=1"><h5 class="mt-0">Nadia</h5></a>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-          <hr>
-          <div class="media py-3">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <a href="/views/Chat.php?otherUser=1"><h5 class="mt-0">Sofia</h5></a>
+              <h5 class="mt-0">Santiago</h5>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
             </div>
           </div>
         </div>
-        <div class="col-3"></div>
+        <div class="col-12">
+          <div class="media py-4 px-5">
+            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+            <div class="media-body">
+              <h5 class="mt-0">Santiago</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="media py-4 px-5">
+            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+            <div class="media-body">
+              <h5 class="mt-0">Santiago</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="media py-4 px-5">
+            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+            <div class="media-body">
+              <h5 class="mt-0">Santiago</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="media py-4 px-5">
+            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+            <div class="media-body">
+              <h5 class="mt-0">Santiago</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <form class="form-inline w-100">
+            <div class="input-group p-5 w-100">
+              <input type="text" class="form-control" name="cuerpo" placeholder="Teclea un mensaje">
+              <div class="input-group-append">
+                  <button class="btn btn-outline-secondary">Button</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-    
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
