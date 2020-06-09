92118e4a135ffb2a247214629318c83e42c7eeca
Santiago Diaz <santidiazg@hotmail.es>
2020-06-08 20:34:28 -0700

    display messages on Chat

--- views/Chat.php
+++ views/Chat.php
@@ -1,3 +1,166 @@
+<?php
+  session_start();
+
+  function startMySQLConnection()
+  {
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $database = "TEConnect";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn)
+    {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $conn;
+  }
+
+  function stopMySQLConnection($conn)
+  {
+    mysqli_close($conn);
+  }
+
+  function numberOfDisplayableMessages($userId1, $userId2)
+  {
+    $conn = startMySQLConnection();
+
+    $sql = "
+      SELECT COUNT(ID_Mensaje) AS count
+      FROM Mensaje
+      WHERE
+        ID_User1 = " . $userId1 . " AND ID_User2 = " . $userId2 . " OR
+        ID_User1 = " . $userId2 . " AND ID_User2 = " . $userId1 . ";
+    ";
+    $result = mysqli_query($conn, $sql);
+    $output = "";
+    if ($row = mysqli_fetch_assoc($result))
+    {
+      $output = $row["count"];
+    }
+
+    stopMySQLConnection($conn);
+
+    return $output;
+  }
+
+  function getMessages($userId1, $userId2)
+  {
+    $conn = startMySQLConnection();
+
+    $sql = "
+      SELECT *
+      FROM Mensaje
+      WHERE
+        ID_User1 = " . $userId1 . " AND ID_User2 = " . $userId2 . " OR
+        ID_User1 = " . $userId2 . " AND ID_User2 = " . $userId1 . "
+      ORDER BY HoraEnviado
+      LIMIT 4;
+    ";
+    $result = mysqli_query($conn, $sql);
+    $output = array();
+    while ($row = mysqli_fetch_assoc($result))
+    {
+      $output[] = $row;
+    }
+
+    stopMySQLConnection($conn);
+
+    return $output;
+  }
+
+  function getUserInfo($userId)
+  {
+    $conn = startMySQLConnection();
+
+    $sql = "
+      SELECT *
+      FROM Usuario
+      WHERE ID_User = " . $userId . ";
+    ";
+    $result = mysqli_query($conn, $sql);
+    $output = array();
+    if ($row = mysqli_fetch_assoc($result))
+    {
+      $output = $row;
+    }
+
+    stopMySQLConnection($conn);
+
+    return $output;
+  }
+
+  function displayMsgSentFromSelf($msg)
+  {
+    $user = getUserInfo($msg["ID_Sender"]);
+
+    echo '<div class="col-2"></div>';
+    echo '<div class="col-8">';
+    echo '<div class="media my-2 mx-5 border rounded shadow-sm p-3 bg-light">';
+    echo '<div class="media-body">';
+    echo '<h5 class="mt-0 text-right">' . $user["PrimerNombre"] . ' ' . $user["Apellido"] . '</h5>';
+    echo '<div class="text-right">' . $msg["Cuerpo"] . '</div>';
+    echo '</div>';
+    echo '<img src="/upload/' . $user["Foto"] . '" class="ml-3" style="max-width: 60px; max-height: 60px; width: auto; height: auto;">';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="col-2"></div>';
+  }
+
+  function displayMsgSentFromOther($msg)
+  {
+    $user = getUserInfo($msg["ID_Sender"]);
+
+    echo '<div class="col-2"></div>';
+    echo '<div class="col-8">';
+    echo '<div class="media my-2 mx-5 border rounded shadow-sm p-3 bg-light">';
+    echo '<img src="/upload/' . $user["Foto"] . '" class="mr-3" style="max-width: 60px; max-height: 60px; width: auto; height: auto;">';
+    echo '<div class="media-body">';
+    echo '<h5 class="mt-0">' . $user["PrimerNombre"] . ' ' . $user["Apellido"] . '</h5>';
+    echo $msg["Cuerpo"];
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="col-2"></div>';
+  }
+
+  function displayMessages($messages)
+  {
+    foreach ($messages as $msg)
+    {
+      if ($msg["ID_Sender"] == $_SESSION["id"])
+      {
+        displayMsgSentFromSelf($msg);
+      }
+      else
+      {
+        displayMsgSentFromOther($msg);
+      }
+    }
+  }
+
+  function displayNoMessages()
+  {
+    echo '<div class="col-12 p-5">';
+    echo '<h1 class="text-center">¡Empieza la conversación enviando un mensaje!</h1>';
+    echo '</div>';
+  }
+
+  function displayContent()
+  {
+    if (numberOfDisplayableMessages($_SESSION["id"], $_GET["User"]) > 0)
+    {
+      $messages = getMessages($_SESSION["id"], $_GET["User"]);
+      displayMessages($messages);
+    }
+    else
+    {
+      displayNoMessages();
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -36,52 +199,11 @@
 
     <div class="container-flex" style="flex: 1 1 auto; overflow-y:hidden;">
       <div class="row" style="position: absolute; bottom: 0; left: 15px; width: 100%; height: auto; overflow-y:hidden;">
-        <div class="col-12">
-          <div class="media py-4 px-5">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <h5 class="mt-0">Santiago</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="media py-4 px-5">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <h5 class="mt-0">Santiago</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="media py-4 px-5">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <h5 class="mt-0">Santiago</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="media py-4 px-5">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <h5 class="mt-0">Santiago</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="media py-4 px-5">
-            <svg class="bd-placeholder-img mr-3" width="64" height="64" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 64x64"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
-            <div class="media-body">
-              <h5 class="mt-0">Santiago</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-            </div>
-          </div>
-        </div>
-        <div class="col-12">
+        <?php
+          displayContent();
+        ?>
+        <div class="col-2"></div>
+        <div class="col-8">
           <form class="form-inline w-100">
             <div class="input-group p-5 w-100">
               <input type="text" class="form-control" name="cuerpo" placeholder="Teclea un mensaje">
@@ -91,6 +213,7 @@
             </div>
           </form>
         </div>
+        <div class="col-2"></div>
       </div>
     </div>
 
