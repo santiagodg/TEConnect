c3522fbcbc92a8eaff2b2a248b20b1b3389fbe6b
Mariano Shaar <>
2020-06-10 23:17:58 -0500

    cambio en diseño editarambito

--- views/EditarAmbito.php
+++ views/EditarAmbito.php
@@ -45,27 +45,69 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="/views/css/styles.css">
-    <link rel="stylesheet" type="text/css" href="/views/css/bootstrap.min.css">
-    <script type="text/javascript" src="/views/js/bootstrap.bundle.min.js"></script>
-    <title>TEConnect | Perfil</title>
-</head>
-<body>
-    <h1>TEConnect</h1>
-    <h2>Editar ambito</h2>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/views/css/bootstrap.min.css">
+
+    <title>TEConnect | Gustos</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index: 2;">
+      <a class="navbar-brand" href="/index.php">TEConnect</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ">
+            <a class="nav-link" href="/views/home.php">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/views/EscogerAmbito.php">Descubrir Personas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/views/Conversaciones.php">Conversaciones</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item mr-3">
+            <a class="nav-link" href="/views/Perfil.php">Perfil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/views/Logout.php">Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container my-5 border rounded bg-light shadow p-3">
+      <div class="row py-5">
+        <div class="col">
+        <!-- <div class="col-1">             BOTÓN VOLVER
+          <a href="#">
+            <svg class="bi bi-arrow-left text-dark" width="60" height="60" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
+              <path fill-rule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+          </a>
+        </div> -->
+        <h4 class="mb-5">Editar Ambito</h1>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
         
             <p>
                 <label>Descripcion del ambito <?php echo $_GET['nombre_ambito'];?></label>
-                <input type="text" name="descripcion" value="<?php echo $descripcion;?>">
+                <input type="text" name="descripcion" class="form-control" value="<?php echo $descripcion;?>">
             </p>
 
             <input type="text" name="action" value="modifyDetalle" style="display:none;">
             <input type="text" name="id_ambito" value="<?php echo $_GET["id_ambito"];?>" style="display:none;">
             <input type="text" name="id_user" value="<?php echo $_SESSION["id"];?>" style="display:none;">
-            <input type="submit" value="Modificar Ambito">
+            <button type="submit" class="btn btn-primary">Modificar Ambito</button>
 
     </form><br>
           <br>
@@ -78,5 +120,14 @@
 
         mysqli_close($conn);
     ?>
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </body>
 </html>
